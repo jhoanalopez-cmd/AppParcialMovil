021a25e7c4375874e4195368e67b60401d6159cf
Jhoana López Jaramillo <jhoanapatricial@gmail.com>
2024-09-13 11:10:05 -0500

    Update 1

---
+++ react-router/src/screens/ConfigScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+export default function ConfigScreen(){
+    return(
+        <View>
+            <Text>Estoy en Config</Text>
+        </View>
+
+    );
+}
